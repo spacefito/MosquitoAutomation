84506d33b0144ed6ab5261d36c7919a77eac2ab0
spacefito <adduarte@sbcglobal.net>
2016-02-09 01:36:43 -0800

    Objectify

--- SpaceCommand/get_in_orbit.py
+++ SpaceCommand/get_in_orbit.py
@@ -3,71 +3,55 @@ import time
 
 import krpc
 
-from space_ship.controls import correct_speed
+from SpaceCommand.space_ship.controls import NavComputer
 
 SPEED_ALTITUDE_RATIO = 40
 APOAPSIS_IN_M = 100000
 STARTING_SPEED_LIMIT = 250
-ENGINE_DELAY=1
-
-def  execute_gravity_turn(vessel):
-    print("Starting gravity turn")
-    vessel.auto_pilot.target_pitch_and_heading(60,90)
-
-def decouple_empty_engines(vessel):
-    engines = []
-    for part in vessel.parts.all:
-        if part.engine:
-            engines.append(part)
-    number_of_empty_egines  = 0
-    for part in engines:
-        if not part.engine.has_fuel:
-            number_of_empty_egines +=1
-        if number_of_empty_egines > 1:
-                number_of_empty_egines  = 0;
-                vessel.control.activate_next_stage()
-
+ENGINE_DELAY = 1
 
 conn = krpc.connect(name="Get in orbit")
-
 vessel = conn.space_center.active_vessel
+navComputer = NavComputer(vessel)
 
 print("Taking over ship")
 vessel.auto_pilot.target_pitch_and_heading(90, 90)
 vessel.auto_pilot.engage()
 throttle_value = 1.0
-print (" Launch in")
-for i in [ "3","2","1","0"]:
+print(" Launch in")
+for i in ["3", "2", "1", "0"]:
     time.sleep(1)
-    print (i+"...")
+    print(i+"...")
 vessel.control.throttle = throttle_value
 gravity_turn_not_initiated = True
-print ("Egine Ignition")
+print("Egine Ignition")
 
 vessel.control.activate_next_stage()
 time.sleep(ENGINE_DELAY)
-print ("Release clamps")
+print("Release clamps")
 vessel.control.activate_next_stage()
 
 speed_limit = STARTING_SPEED_LIMIT
 
 # First four three stages are engines.
-while vessel.control.current_stage >4:
+while vessel.control.current_stage > 4:
     if gravity_turn_not_initiated and vessel.flight().mean_altitude > 10000:
-        execute_gravity_turn(vessel)
+        navComputer.execute_gravity_turn()
         gravity_turn_not_initiated = False
-    correct_speed(vessel,speed_limit)
-    decouple_empty_engines(vessel)
+    navComputer.correct_speed(speed_limit,1)
+    navComputer.decouple_empty_engines(2)
 
 # now we head for apoapsis
-print ("Waiting to reach apoapsis of %s" %APOAPSIS_IN_M)
-# At this point we check apoapsis and continue to accelerate until we get what we want
+print("Waiting to reach apoapsis of %s" % APOAPSIS_IN_M)
+# At this point we check apoapsis and continue to accelerate until we get what
+# we want
 while vessel.orbit.apoapsis_altitude < APOAPSIS_IN_M:
-    correct_speed(vessel,speed_limit)
     speed_limit = vessel.flight().mean_altitude / SPEED_ALTITUDE_RATIO
+    navComputer.correct_speed(speed_limit,0.5)
 
-print ("Projected vessel apoappsis: ", vessel.orbit.apoapsis_altitude)
-print ("Cutting throttle off!")
+
+print("Projected vessel apoappsis: ", vessel.orbit.apoapsis_altitude)
+print("Cutting throttle off!")
 # at this point cut thrust to a minimum so we don't loose control
 throttle_value = 0.001
 vessel.control.throttle = throttle_value
@@ -75,50 +59,36 @@ vessel.control.throttle = throttle_value
 desired_orbit_reached = False
 while not desired_orbit_reached:
     degrees_above_horizon = 10
-    sys.stdout.write("\rTime to apoapsis: %i s" % vessel.orbit.time_to_apoapsis)
+    sys.stdout.write("\rTime to apoapsis: %i s"
+                     % vessel.orbit.time_to_apoapsis)
     sys.stdout.flush()
     if vessel.orbit.time_to_apoapsis < 30:
-        throttle_value= 0.10
+        throttle_value = 0.10
         degrees_above_horizon = 10
     if vessel.orbit.time_to_apoapsis < 20:
-        throttle_value= 0.20
+        throttle_value = 0.20
         degrees_above_horizon = 10
     if vessel.orbit.time_to_apoapsis < 10:
-        throttle_value= 0.75
+        throttle_value = 0.75
         degrees_above_horizon = 15
     if vessel.orbit.time_to_apoapsis < 7:
-        throttle_value= 1
+        throttle_value = 1
         degrees_above_horizon = 15
     if vessel.orbit.time_to_apoapsis > 45:
         throttle_value = 0
 
-    vessel.auto_pilot.target_pitch_and_heading(degrees_above_horizon ,90)
-
+    vessel.auto_pilot.target_pitch_and_heading(degrees_above_horizon, 90)
     vessel.control.throttle = throttle_value
-
-
-
     acceptable_error = vessel.orbit.apoapsis_altitude * .01
-    if (vessel.orbit.apoapsis_altitude - vessel.orbit.periapsis_altitude) < acceptable_error:
+    if ((vessel.orbit.apoapsis_altitude - vessel.orbit.periapsis_altitude) <
+            acceptable_error):
         desired_orbit_reached = True
-
-    engines = []
-    number_of_empty_egines = 0
-    for part in vessel.parts.all:
-        if part.engine:
-            engines.append(part)
-    for part in engines:
-        if not part.engine.has_fuel:
-            number_of_empty_egines +=1
-        if number_of_empty_egines > 0:
-                number_of_empty_egines  = 0;
-                vessel.control.activate_next_stage()
-                print ("current stage: ", vessel.control.current_stage)
+    navComputer.decouple_empty_engines(1)
 
 vessel.control.throttle = 0
 if desired_orbit_reached:
-    print ("success!")
-
+    print("success!")
+conn.close()
 
 
 
--- SpaceCommand/space_ship/controls.py
+++ SpaceCommand/space_ship/controls.py
@@ -1,41 +1,103 @@
 import time
-def calculate_desired_g_force(vessel_speed, target_speed, delta_t):
+import sys
 
-    delta_v  = target_speed - vessel_speed
-    desired_acceleration  = delta_v/delta_t
-    desired_g_force = desired_acceleration/9.8
-    if desired_g_force < 0:
-        desired_g_force = 0
-    return desired_g_force
+class NavComputer(object):
+    def __init__(self, vessel):
+        self._vessel = vessel
 
-def correct_speed(vessel, target_speed, delay=0):
-    time.sleep(delay)
-    throttle_value  = vessel.control.throttle
-    g_force = vessel.flight().g_force
-    margin = 0.1
-    acceleration_time = 2
+    @property
+    def vessel(self):
+        return self._vessel
 
-    # if vessel speed is below our target speed AND the g force is not accelerating it
-    # we have to increase the throttle until g_force is large enough
-    vessel_speed = vessel.flight(vessel.orbit.body.reference_frame).speed
-    desired_g_force = calculate_desired_g_force(vessel_speed,target_speed, acceleration_time)
-    if g_force >0:
-        g_force_ratio = desired_g_force / g_force
-    else:
-        g_force_ration = -1.0
+    @vessel.setter
+    def vessel(self, vessel):
+        self._vessel = vessel
 
-    if vessel_speed < target_speed:
-        if g_force < desired_g_force:
-            ratio = target_speed / vessel_speed
-            throttle_value = 0.5 + (throttle_value + (1- throttle_value)* ratio)
-            if throttle_value > 1:
-                throttle_value = 1.0
-            print ("increasing throttle to")
-            vessel.control.throttle = throttle_value
+    @staticmethod
+    def calculate_desired_g_force(vessel_speed, target_speed, delta_t):
 
-    # if we are going faster than the target speed, we cut down on the throtle
-    elif vessel_speed > target_speed * (1 + margin):
-        throttle_value= vessel.control.throttle / 2
-        print ("reducing throtle to %s "% throttle_value)
-        vessel.control.throttle = throttle_value
+        delta_v = target_speed - vessel_speed
+        desired_acceleration = delta_v/delta_t
+        desired_g_force = desired_acceleration/9.8
+        if desired_g_force < 0:
+            desired_g_force = 0
+        return desired_g_force
 
+    def correct_speed(self, target_speed, delay=0):
+        time.sleep(delay)
+        throttle_value = self._vessel.control.throttle
+        g_force = self.vessel.flight().g_force
+        margin = 0.1
+        acceleration_time = 2
+
+        # if vessel speed is below our target speed AND the g force is not
+        # accelerating it we have to increase the throttle until g_force is
+        # large enough
+        vessel_speed = self._vessel.flight(
+            self._vessel.orbit.body.reference_frame).speed
+        desired_g_force = self.calculate_desired_g_force(vessel_speed,
+                                                         target_speed,
+                                                         acceleration_time)
+        if vessel_speed < target_speed:
+            if g_force < desired_g_force:
+                ratio = target_speed / vessel_speed
+                throttle_value = (throttle_value +
+                                        (1 - throttle_value) / ratio)
+                if throttle_value > 1:
+                    throttle_value = 1.0
+                self._vessel.control.throttle = throttle_value
+
+        # if we are going faster than the target speed, we cut down on
+        # the throttle
+        elif vessel_speed > target_speed * (1 + margin):
+            throttle_value = self._vessel.control.throttle / 2
+            self._vessel.control.throttle = throttle_value
+
+    def execute_gravity_turn(self):
+        print("Starting gravity turn")
+        self.vessel.auto_pilot.target_pitch_and_heading(60, 90)
+
+    def decouple_empty_engines(self, number_of_engines_to_decouple=1):
+        engines = []
+        for part in self.vessel.parts.all:
+            if part.engine:
+                engines.append(part)
+        number_of_empty_egines = 0
+        for part in engines:
+            if not part.engine.has_fuel:
+                number_of_empty_egines += 1
+            if number_of_empty_egines > number_of_engines_to_decouple - 1:
+                self.vessel.control.activate_next_stage()
+                return True
+        return False
+
+    def chase_the_apoapsis(self):
+        while not desired_orbit_reached:
+            degrees_above_horizon = 10
+            sys.stdout.write("\rTime to apoapsis: %i s"
+                             % self.vessel.orbit.time_to_apoapsis)
+            sys.stdout.flush()
+            if self.vessel.orbit.time_to_apoapsis < 30:
+                throttle_value = 0.10
+                degrees_above_horizon = 10
+            if self.vessel.orbit.time_to_apoapsis < 20:
+                throttle_value = 0.20
+                degrees_above_horizon = 10
+            if self.vessel.orbit.time_to_apoapsis < 10:
+                throttle_value = 0.75
+                degrees_above_horizon = 15
+            if self.vessel.orbit.time_to_apoapsis < 7:
+                throttle_value = 1
+                degrees_above_horizon = 15
+            if self.vessel.orbit.time_to_apoapsis > 45:
+                throttle_value = 0
+
+            self._vessel.auto_pilot.target_pitch_and_heading(degrees_above_horizon, 90)
+            self.vessel.control.throttle = throttle_value
+            acceptable_error = vessel.orbit.apoapsis_altitude * .01
+            if ((self.vessel.orbit.apoapsis_altitude
+                     - self.vessel.orbit.periapsis_altitude) <
+                    acceptable_error):
+                desired_orbit_reached = True
+            if(self.decouple_empty_engines(1)):
+                self.vessel.control.activate_next_stage()
